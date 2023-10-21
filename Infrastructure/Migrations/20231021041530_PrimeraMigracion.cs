dcdc0683b0d1404164b46dc330f955a43a8ff305
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2023-10-20 23:27:12 -0500

    Configurando migracion

---
+++ Infrastructure/Migrations/20231021041530_PrimeraMigracion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Serie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlImagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Emision = table.Column<bool>(type: "bit", nullable: false),
+                    Capitulos = table.Column<int>(type: "int", nullable: false),
+                    CapitulosVistos = table.Column<int>(type: "int", nullable: false),
+                    DondeVer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Serie", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Serie");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Calendar-Series-Backend
+
+# Install BD With Docker
+
+# 1 Download Image from docker hub
+
+``` docker pull mcr.microsoft.com/mssql/server ```
+
+``` docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -e "MSSQL_PID=Express" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest ```
+
+
+# Connection in .Net 
+
+``` "DefaultConnection": "Data Source=sql-server,1433;Initial Catalog=CalendarSeries;User Id=sa;Password=Prueba123*;TrustServerCertificate=true" ```
+
+
+# Create Migration Databasek
+
+Ir a Infrastructure en la terminal y ejecutar el siguiente comando en caso de no existir la migración
+
+``` dotnet ef migrations add PrimeraMigracion --startup-project ../Presentation/Presentation.csproj --context CalendarSerieContext ```
+
+Ahora es importante ejecutar el siguiente comando para que se ejecute el script creado en la migración.
+
+``` dotnet ef database update --context CalendarSerieContext --startup-project ../Presentation/Presentation.csproj ```
+
+# Excluir archivos autogenerados de git
+
+``` git rm -r --cached Infrastructure/obj/ ```
+
